c17974de01d09603fd686bb2c75281677319590a
bellring <starringbell@gmail.com>
2023-10-26 00:26:08 +0900

    webclient

---
+++ mvc/src/main/java/com/example/mvc/MvcApplication.java
@@ -0,0 +1,22 @@
+package com.example.mvc;
+
+import java.util.Map;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcApplication.class, args);
+    }
+
+    @GetMapping("/posts/{id}")
+    public Map<String, String> getPosts(@PathVariable Long id) {
+        return Map.of("id", id.toString(), "content", "Posts content is %d".formatted(id));
+    }
+}
---
+++ webflux1/src/main/java/com/example/webflux1/service/PostService.java
@@ -0,0 +1,19 @@
+package com.example.webflux1.service;
+
+import com.example.webflux1.client.PostClient;
+import com.example.webflux1.dto.PostResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+    // webclient mvc server request
+    private final PostClient postClient;
+
+    public Mono<PostResponse> getPostContent(Long id) {
+        return postClient.getPost(id);
+    }
+
+}
